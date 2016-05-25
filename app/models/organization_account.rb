9c8c2ed1b65e72b1f0fde2b48398c15741184c17
madmatvey <potehin@gmail.com>
2016-05-25 19:19:54 +0400

    add view and policy for organization account

--- app/models/organization_account.rb
+++ app/models/organization_account.rb
@@ -2,4 +2,7 @@ class OrganizationAccount < ActiveRecord::Base
   validates :name, :presence => true,
                     :length => { :minimum => 3 }
   has_many :users
+  # def @names
+  #
+  # end
 end
