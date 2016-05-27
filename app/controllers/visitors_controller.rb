e17c2f969fe8160e994703a29870d8a64c6e9251
madmatvey <potehin@gmail.com>
2016-05-27 13:11:29 +0400

    add i18n +++

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,2 @@
 class VisitorsController < ApplicationController
-  def about
-    # render "pages/about"
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
-  # get "/about/" => "visitors#about"
   devise_for :users
   resources :users
 end
