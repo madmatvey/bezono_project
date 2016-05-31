d9ea8b1504bf2c222d627c981cd0a31b86d23014
madmatvey <potehin@gmail.com>
2016-06-01 00:03:13 +0400

    seed account added

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+demo_account = OrganizationAccount.create(name: 'demo_account')
+user.organization_account = demo_account
+user.save
