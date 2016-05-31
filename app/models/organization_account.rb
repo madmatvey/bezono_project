856dc308e353dd24ab2f6df42c37e6df0192d930
madmatvey <potehin@gmail.com>
2016-06-01 00:28:47 +0400

    view .authform

--- app/models/organization_account.rb
+++ app/models/organization_account.rb
@@ -1,9 +1,6 @@
 class OrganizationAccount < ActiveRecord::Base
-  validates :name, :presence => true,
-                    :length => { :minimum => 3 }
+  validates :name, :presence => true, :length => { :minimum => 3 }
+  validates :name, uniqueness: { case_sensitive: false }
   has_many :users
   has_one :organization_profile
-
-
-
 end
