61e0740ddb988b3fa8e2a56f6ba2912df45261cc
Evgeniy Leontiev <potehin@gmail.com>
2016-06-10 10:40:47 +0400

    add callback for profile to set default accreditation

--- app/models/organization_profile.rb
+++ app/models/organization_profile.rb
@@ -1,4 +1,9 @@
 class OrganizationProfile < ActiveRecord::Base
   belongs_to :organization_account
   has_one :accreditation
+  after_initialize :set_default_accreditation, :if => :new_record?
+
+  def set_default_accreditation
+    self.accreditation ||= Accreditation.create
+  end
 end
