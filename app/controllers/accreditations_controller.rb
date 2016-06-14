858516a702c6456f9b205b6f4c6abf8c94f6583a
Evgeniy Leontiev <potehin@gmail.com>
2016-06-14 13:59:17 +0400

    accreditation added to account view and 3 fake steps goes on

--- app/controllers/accreditations_controller.rb
+++ app/controllers/accreditations_controller.rb
@@ -2,35 +2,36 @@ class AccreditationsController < ApplicationController
   # include Wicked::Wizard
 
 
-#   def new
-#     @accreditation = Accreditation.new
-#   end
-#
+  def new
+    @accreditation = Accreditation.new
+  end
+
   def edit
     @accreditation = Accreditation.find(accreditation_params)
   end
 
-#   def show
-#     @accreditation = Accreditation.find(secure_params)
-#
-#
+  def show
+    @accreditation = Accreditation.find(secure_params)
+  end
+
 
   def update
 
     @accreditation = find_accreditation
 
     if @accreditation.update_attributes(accreditation_params)
+      flash[:success] = "Accreditation updated!"
       redirect_to ([@accreditation.organization_profile, @accreditation])
     else
       render :action => "edit"
+    end
   end
-end
 
 private
 
-def accreditation_params
-  params.require(:accreditation).permit(:accreditation_id, :id, :state, :organization_profile_id)
-end
+  def accreditation_params
+    params.require(:accreditation).permit(:accreditation_id, :id, :state, :organization_profile_id)
+  end
 
   def find_accreditation
     if params[:accreditation_id]
--- app/models/accreditation.rb
+++ app/models/accreditation.rb
@@ -37,37 +37,13 @@ class Accreditation < ActiveRecord::Base
 
   has_state_history
 
-  # attr_writer :current_step
-  #
-  # def current_step
-  #   @current_step || steps.first
-  # end
-  #
-  # def steps
-  #   %w[first second confirmation]
-  # end
-  #
-  # def next_step
-  #   self.current_step = steps[steps.index(current_step)+1]
-  # end
-  #
-  # def previous_step
-  #   self.current_step = steps[steps.index(current_step)-1]
-  # end
-  #
-  # def first_step?
-  #   current_step == steps.first
-  # end
-  #
-  # def last_step?
-  #   current_step == steps.last
-  # end
-  #
-  # def all_valid?
-  #   steps.all? do |step|
-  #     self.current_step = step
-  #     valid?
-  #   end
-  # end
+  def state= value
+    if value.kind_of?(String) and value.to_i.to_s == value
+      super value.to_i
+    else
+      super value
+    end
+  end
+
 
 end
