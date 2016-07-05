41f3f7d349fdd9652e701f952107c81419398c5b
Evgeniy Leontiev <potehin@gmail.com>
2016-07-05 13:52:09 +0400

    refactoring of JS code in demands for competences manipulating

--- app/assets/javascripts/demands.coffee
+++ app/assets/javascripts/demands.coffee
@@ -1,3 +1,51 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+(($) ->
+  $ ->
+    $('#not-choosed-compenences').on 'click', '.btn.btn-info.btn-sm', (event) ->
+      div_id = this.id
+      arr = div_id.split('_')
+      comp_id = arr[arr.length - 1]
+
+      array_val = []
+      value = $("#demand_competence_ids").val()
+      array_val = value.split(",")
+      array_val.push(comp_id)
+      $("#demand_competence_ids").val(array_val)
+      $('#group_'+comp_id).detach().prependTo('#choosed-compenences')
+      $('#add_competence_'+comp_id).replaceWith("<div class='btn btn-success btn-sm' id='delete_competence_"+comp_id+"'><i class='fa fa-check fa-lg'></i></div>")
+
+      console.log 'click on not choosed element with id: ' + comp_id
+      return
+    $('#choosed-compenences').on 'click', '.btn.btn-success.btn-sm', (event) ->
+      div_id = this.id
+      arr = div_id.split('_')
+      comp_id = arr[arr.length - 1]
+
+      value = $("#demand_competence_ids").val()
+      array_val = value.split(",")
+      array_val.pop(comp_id)
+      $("#demand_competence_ids").val(array_val)
+      $('#group_'+comp_id).detach().prependTo('#not-choosed-compenences')
+      $('#delete_competence_'+comp_id).replaceWith( "<div class='btn btn-info btn-sm' id='add_competence_"+comp_id+"'><i class='fa fa-plus fa-lg'></i></div>")
+
+
+      console.log 'click on choosed element with id: ' + comp_id
+      return
+    $('.btn.btn-success.btn-sm').hover (->
+      $(this).children().replaceWith("<i class='fa fa-minus fa-lg'>")
+      div_id = this.id
+      arr = div_id.split('_')
+      comp_id = arr[arr.length - 1]
+      console.log 'mouse on choosed element with id: ' + comp_id
+      return
+      ), ->
+        $(this).children().replaceWith("<i class='fa fa-check fa-lg'>")
+        div_id = this.id
+        arr = div_id.split('_')
+        comp_id = arr[arr.length - 1]
+        console.log 'mouse out of choosed element with id: ' + comp_id
+        return
+    return
+) jQuery
--- public/assets/application-d082482df161559e1fdbec7e665c0f6bff899b5f1f1331d964d684671850fc1c.js
+++ public/assets/application-d082482df161559e1fdbec7e665c0f6bff899b5f1f1331d964d684671850fc1c.js
@@ -20655,7 +20655,52 @@ $(document).on('ready page:change', function() {
 
 }).call(this);
 (function() {
-
+  (function($) {
+    return $(function() {
+      $('#not-choosed-compenences').on('click', '.btn.btn-info.btn-sm', function(event) {
+        var arr, array_val, comp_id, div_id, value;
+        div_id = this.id;
+        arr = div_id.split('_');
+        comp_id = arr[arr.length - 1];
+        array_val = [];
+        value = $("#demand_competence_ids").val();
+        array_val = value.split(",");
+        array_val.push(comp_id);
+        $("#demand_competence_ids").val(array_val);
+        $('#group_' + comp_id).detach().prependTo('#choosed-compenences');
+        $('#add_competence_' + comp_id).replaceWith("<div class='btn btn-success btn-sm' id='delete_competence_" + comp_id + "'><i class='fa fa-check fa-lg'></i></div>");
+        console.log('click on not choosed element with id: ' + comp_id);
+      });
+      $('#choosed-compenences').on('click', '.btn.btn-success.btn-sm', function(event) {
+        var arr, array_val, comp_id, div_id, value;
+        div_id = this.id;
+        arr = div_id.split('_');
+        comp_id = arr[arr.length - 1];
+        value = $("#demand_competence_ids").val();
+        array_val = value.split(",");
+        array_val.pop(comp_id);
+        $("#demand_competence_ids").val(array_val);
+        $('#group_' + comp_id).detach().prependTo('#not-choosed-compenences');
+        $('#delete_competence_' + comp_id).replaceWith("<div class='btn btn-info btn-sm' id='add_competence_" + comp_id + "'><i class='fa fa-plus fa-lg'></i></div>");
+        console.log('click on choosed element with id: ' + comp_id);
+      });
+      $('.btn.btn-success.btn-sm').hover((function() {
+        var arr, comp_id, div_id;
+        $(this).children().replaceWith("<i class='fa fa-minus fa-lg'>");
+        div_id = this.id;
+        arr = div_id.split('_');
+        comp_id = arr[arr.length - 1];
+        console.log('mouse on choosed element with id: ' + comp_id);
+      }), function() {
+        var arr, comp_id, div_id;
+        $(this).children().replaceWith("<i class='fa fa-check fa-lg'>");
+        div_id = this.id;
+        arr = div_id.split('_');
+        comp_id = arr[arr.length - 1];
+        console.log('mouse out of choosed element with id: ' + comp_id);
+      });
+    });
+  })(jQuery);
 
 }).call(this);
 (function() {
