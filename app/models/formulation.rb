153ba52d72a4a694116d8ffb05184adb95968dad
Evgeniy Leontiev <potehin@gmail.com>
2016-07-14 12:15:10 +0400

    model formulation refrences

--- app/models/formulation.rb
+++ app/models/formulation.rb
@@ -1,5 +1,6 @@
 class Formulation < ActiveRecord::Base
   belongs_to :problem
   belongs_to :formulation
+  belongs_to :organization_profile
   validates :content, presence: true
 end
--- db/migrate/20160714080022_create_formulations.rb
+++ db/migrate/20160714080022_create_formulations.rb
@@ -2,6 +2,7 @@ class CreateFormulations < ActiveRecord::Migration
   def change
     create_table :formulations do |t|
       t.references :problem, index: true, foreign_key: true, null: false
+      t.references :organization_profile, index: true, foreign_key: true, null: false
       t.references :formulation, index: true, foreign_key: true
       t.text :content
 
