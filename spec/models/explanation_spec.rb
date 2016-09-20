aa22fb998af38d6f8d6d52b94b6c49914ed1e9d7
Evgeniy Leontiev <potehin@gmail.com>
2016-09-20 09:14:27 +0400

    testin: explanation model validates

--- spec/models/explanation_spec.rb
+++ spec/models/explanation_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Explanation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    expect(Explanation.new( message: "message",
+                            user_id: 1,
+                            organization_profile_id: 1,
+                            demand_id: 1)).to be_valid
+  end
+  it "is not valid without a message" do
+    expect(Explanation.new( user_id: 1,
+                            organization_profile_id: 1,
+                            demand_id: 1)).to_not be_valid
+  end
+  it "is not valid without a user_id" do
+    expect(Explanation.new( message: "message",
+                            organization_profile_id: 1,
+                            demand_id: 1)).to_not be_valid
+  end
+  it "is not valid without a organization_profile_id" do
+    expect(Explanation.new( message: "message",
+                            user_id: 1,
+                            demand_id: 1)).to_not be_valid
+  end
+  it "is not valid without a demand_id" do
+    expect(Explanation.new( message: "message",
+                            user_id: 1,
+                            organization_profile_id: 1)).to_not be_valid
+  end
 end
