4c382ee17357e8f7be6cc4cd5c9a976c0e70488f
Evgeniy Leontiev <potehin@gmail.com>
2016-09-26 16:56:39 +0400

    Explanations.to_react covered

--- spec/models/explanation_spec.rb
+++ spec/models/explanation_spec.rb
@@ -32,9 +32,94 @@ describe Explanation, '#account' do
   end
 end
 
-# describe Explanation, '#answered?' do
-#   it 'returns true if explanation have answer' do
-#     explanation = FactoryGirl.create(:explanation)
-#     expect(explanation.account).to be_an_instance_of(OrganizationAccount)
-#   end
-# end
+describe Explanation, '.to_react(demand)' do
+  it 'returns Array with explanations' do
+    demand = FactoryGirl.create(:demand)
+    exp1 = FactoryGirl.create(:explanation)
+    exp2 = FactoryGirl.create(:explanation)
+    demand.explanations << exp1
+    demand.explanations << exp2
+    result = Explanation.to_react(demand)
+    expect(result).to be_an_instance_of(Array)
+  end
+  it 'returns id in JSON' do
+    demand = FactoryGirl.create(:demand)
+    exp1 = FactoryGirl.create(:explanation)
+    exp2 = FactoryGirl.create(:explanation)
+    demand.explanations << exp1
+    demand.explanations << exp2
+    result = Explanation.to_react(demand)
+    expect(result.first["id"]).to be exp1.id
+    #{"id"=>8, "message"=>"Tempora error omnis rerum pariatur. №9", "user_id"=>15, "demand_id"=>11, "organization_profile_id"=>95, "question_id"=>nil, "from"=>"zero", "answer_id"=>nil}
+  end
+  it 'returns message in JSON' do
+    demand = FactoryGirl.create(:demand)
+    exp1 = FactoryGirl.create(:explanation)
+    exp2 = FactoryGirl.create(:explanation)
+    demand.explanations << exp1
+    demand.explanations << exp2
+    result = Explanation.to_react(demand)
+    expect(result.first["message"]).to be exp1.message
+    #{"id"=>8, "message"=>"Tempora error omnis rerum pariatur. №9", "user_id"=>15, "demand_id"=>11, "organization_profile_id"=>95, "question_id"=>nil, "from"=>"zero", "answer_id"=>nil}
+  end
+  it 'returns user_id in JSON' do
+    demand = FactoryGirl.create(:demand)
+    exp1 = FactoryGirl.create(:explanation)
+    exp2 = FactoryGirl.create(:explanation)
+    demand.explanations << exp1
+    demand.explanations << exp2
+    result = Explanation.to_react(demand)
+    expect(result.first["user_id"]).to be exp1.user_id
+    #{"id"=>8, "message"=>"Tempora error omnis rerum pariatur. №9", "user_id"=>15, "demand_id"=>11, "organization_profile_id"=>95, "question_id"=>nil, "from"=>"zero", "answer_id"=>nil}
+  end
+  it 'returns demand_id in JSON' do
+    demand = FactoryGirl.create(:demand)
+    exp1 = FactoryGirl.create(:explanation)
+    exp2 = FactoryGirl.create(:explanation)
+    demand.explanations << exp1
+    demand.explanations << exp2
+    result = Explanation.to_react(demand)
+    expect(result.first["demand_id"]).to be exp1.demand_id
+    #"organization_profile_id"=>95, "question_id"=>nil, "from"=>"zero", "answer_id"=>nil}
+  end
+  it 'returns organization_profile_id in JSON' do
+    demand = FactoryGirl.create(:demand)
+    exp1 = FactoryGirl.create(:explanation)
+    exp2 = FactoryGirl.create(:explanation)
+    demand.explanations << exp1
+    demand.explanations << exp2
+    result = Explanation.to_react(demand)
+    expect(result.first["organization_profile_id"]).to be exp1.organization_profile_id
+    #"organization_profile_id"=>95, "question_id"=>nil, "from"=>"zero", "answer_id"=>nil}
+  end
+  it 'returns question_id in JSON' do
+    demand = FactoryGirl.create(:demand)
+    exp1 = FactoryGirl.create(:explanation)
+    exp2 = FactoryGirl.create(:explanation)
+    demand.explanations << exp1
+    demand.explanations << exp2
+    result = Explanation.to_react(demand)
+    expect(result.first["question_id"]).to be exp1.question_id
+    # "from"=>"zero", "answer_id"=>nil}
+  end
+  # it 'returns "from" in JSON' do
+  #   demand = FactoryGirl.create(:demand)
+  #   exp1 = FactoryGirl.create(:explanation)
+  #   exp2 = FactoryGirl.create(:explanation)
+  #   demand.explanations << exp1
+  #   demand.explanations << exp2
+  #   result = Explanation.to_react(demand)
+  #   expect(result.first["from"]).to be exp1.from
+  #   # "from"=>"zero", "answer_id"=>nil}
+  # end
+  # it 'returns "answer_id" in JSON' do
+  #   demand = FactoryGirl.create(:demand)
+  #   exp1 = FactoryGirl.create(:explanation)
+  #   exp2 = FactoryGirl.create(:explanation)
+  #   demand.explanations << exp1
+  #   demand.explanations << exp2
+  #   result = Explanation.to_react(demand)
+  #   expect(result.first["answer_id"]).to be exp1.answer_id
+  #
+  # end
+end
