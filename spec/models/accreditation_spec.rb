d7d5580e3233c3865ebd27426080dd212a7839f2
Evgeniy Leontiev <potehin@gmail.com>
2016-09-26 16:04:47 +0400

    Accreditation model tests covered

--- spec/models/accreditation_spec.rb
+++ spec/models/accreditation_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-# RSpec.describe Accreditation, type: :model do
-#
-# end
+describe Accreditation, '#state= value' do
+  it 'set state from digit' do
+    accreditation = FactoryGirl.create(:accreditation)
+    accreditation.state = 1
+    expect(accreditation.state).to eq "supplier"
+  end
+  it 'set state from string' do
+    accreditation = FactoryGirl.create(:accreditation)
+    accreditation.state = "supplier"
+    expect(accreditation.state).to eq "supplier"
+  end
+end
+
+describe Accreditation, '.verified' do
+  it 'select only verified from all' do
+    accreditation1 = FactoryGirl.create(:accreditation,:supplier)
+    accreditation2 = FactoryGirl.create(:accreditation,:customer)
+    accreditation3 = FactoryGirl.create(:accreditation,:partner)
+    accreditation4 = FactoryGirl.create(:accreditation,:zero)
+    accreditation5 = FactoryGirl.create(:accreditation,:expired)
+    expect(Accreditation.verified).to match_array([accreditation1,accreditation2,accreditation3])
+  end
+end
