fb38aff92a5ceddf15b4fb23e7c4e582bc5f88a3
Evgeniy Leontiev <potehin@gmail.com>
2016-09-27 14:17:33 +0400

    Explanation model covered

--- spec/models/explanation_spec.rb
+++ spec/models/explanation_spec.rb
@@ -102,24 +102,31 @@ describe Explanation, '.to_react(demand)' do
     expect(result.first["question_id"]).to be exp1.question_id
     # "from"=>"zero", "answer_id"=>nil}
   end
-  # it 'returns "from" in JSON' do
-  #   demand = FactoryGirl.create(:demand)
-  #   exp1 = FactoryGirl.create(:explanation)
-  #   exp2 = FactoryGirl.create(:explanation)
-  #   demand.explanations << exp1
-  #   demand.explanations << exp2
-  #   result = Explanation.to_react(demand)
-  #   expect(result.first["from"]).to be exp1.from
-  #   # "from"=>"zero", "answer_id"=>nil}
-  # end
-  # it 'returns "answer_id" in JSON' do
-  #   demand = FactoryGirl.create(:demand)
-  #   exp1 = FactoryGirl.create(:explanation)
-  #   exp2 = FactoryGirl.create(:explanation)
-  #   demand.explanations << exp1
-  #   demand.explanations << exp2
-  #   result = Explanation.to_react(demand)
-  #   expect(result.first["answer_id"]).to be exp1.answer_id
-  #
-  # end
+end
+
+describe Explanation, '.answered' do
+  it 'returns array of answered Explantions' do
+    exp1 = FactoryGirl.create(:explanation)
+    exp2 = FactoryGirl.create(:explanation)
+    exp1.answer = exp2
+    expect(Explanation.answered).to match_array([exp1])
+  end
+end
+
+describe Explanation, '#from_supplier?' do
+  it 'return TRUE if org is supplier' do
+    profile = FactoryGirl.create(:organization_profile, :accreditation_supplier)
+    exp = FactoryGirl.create(:explanation)
+    exp.organization_profile = profile
+    expect(exp.from_supplier?).to be_truthy
+  end
+end
+
+describe Explanation, '#from_customer?' do
+  it 'return TRUE if org is customer' do
+    profile = FactoryGirl.create(:organization_profile, :accreditation_customer)
+    exp = FactoryGirl.create(:explanation)
+    exp.organization_profile = profile
+    expect(exp.from_customer?).to be_truthy
+  end
 end
